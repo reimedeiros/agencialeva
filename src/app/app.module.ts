8ff5ee4bd387aec42fad8d89cbcb55b496c45e52
Rei <rm091291@gmail.com>
2022-09-05 22:54:00 -0300

    Versionamento + instalação do botstrapp e montagem da estrutura das section 1 e 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { IconsModule } from './icons/icons.module';
 
-const MODULES = [BrowserModule, AppRoutingModule, NgbModule, IconsModule];
+const MODULES = [BrowserModule, AppRoutingModule, IconsModule];
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [MODULES],
